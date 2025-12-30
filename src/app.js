40a2317b23972f378b2fc55c818e051518a6dd2a
ChutidetBen-oss <chutidet.benchathamloed@gmail.com>
2025-12-30 11:04:21 +0700

    refactored to use centralized error handeling

--- src/app.js
+++ src/app.js
@@ -18,3 +18,24 @@ app.use(cors(corsOption))
 app.use(express.json());
 
 app.use("/api", apiRouter)
+
+// Catch-all for 404 Not found
+app.use((req, res, next) => {
+    new Error(`Not found: ${req.method} ${req.originalUrl}`)
+    error.name = "NotFoundError"
+    error.status = 404
+    next(error)
+})
+
+// Centralized Error Handlind Middlewere
+app.use((err, req, res, nexf) => {
+    console.error(err.stack);
+    res.status(err.status || 500).json({
+        seccess: false,
+        message: err.message || "Internal Server Error",
+        path:req.originalUrl,
+        method:req.method,
+        timestamp:new Data().toISOString(),
+        stack:err.stack,
+    })
+})
