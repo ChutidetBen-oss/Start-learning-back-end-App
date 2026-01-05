737bafe7c1d41ed8ed7fda0f1a0a6534ebdf94f1
ChutidetBen-oss <chutidet.benchathamloed@gmail.com>
2026-01-05 16:23:04 +0700

    feat:log in safety

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors"
 import {router as apiRouter} from "./routes/index.js"
+import cookieParser from "cookie-parser"
 
 export const app = express();
 
@@ -17,25 +18,27 @@ app.use(cors(corsOption))
 
 app.use(express.json());
 
+app.use(cookieParser())
+
 app.use("/api", apiRouter)
 
 // Catch-all for 404 Not found
 app.use((req, res, next) => {
-    new Error(`Not found: ${req.method} ${req.originalUrl}`)
+    const error = new Error(`Not found: ${req.method} ${req.originalUrl}`)
     error.name = "NotFoundError"
     error.status = 404
     next(error)
 })
 
 // Centralized Error Handlind Middlewere
-app.use((err, req, res, nexf) => {
+app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(err.status || 500).json({
         seccess: false,
         message: err.message || "Internal Server Error",
         path:req.originalUrl,
         method:req.method,
-        timestamp:new Data().toISOString(),
+        timestamp:new Date().toISOString(),
         stack:err.stack,
     })
 })
--- src/routes/v2/users.routes.js
+++ src/routes/v2/users.routes.js
@@ -4,12 +4,14 @@ import {
     createUser2,
     deleteUser2,
     updateUser2,
-    getUser2
+    getUser2,
 } from "../../module/users/users.controller.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import {User}  from "../../module/users/users.model.js";
+import {authUser} from "../../middlewares/auth.js"
 
-
-export const router = Router()
-
+export const router = Router();
 
 router.get("/", getUsers2);
 
@@ -20,4 +22,119 @@ router.post("/", createUser2);
 // The function inside ia called "Router Handler or Controller"
 router.delete("/:id", deleteUser2);
 
-router.patch("/:id", updateUser2)
+router.patch("/:id", updateUser2);
+
+//Login a user - jwt signed token (token in cookies)
+router.post("/auth/cookie/login", async (req, res, next) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return res.status(400).json({
+            error: true,
+            message: "Email and Password are required",
+        });
+    }
+
+    try {
+        const normalizedEmail = String(email).trim().toLowerCase();
+
+        const user = await User.findOne({ email: normalizedEmail }).select(
+            "+password"
+        );
+
+        if (!user) {
+            return res.status(401).json({
+                error: true,
+                message: "User not found...",
+            });
+        }
+
+        const isMatched = await bcrypt.compare(password, user.password);
+
+        if (!isMatched) {
+            return res.status(401).json({
+                error: true,
+                messege: "Invalid password...",
+            });
+        }
+
+        //Generat JSON Wed Token
+        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+            expiresIn: "10m",
+        });
+
+        const isProd = process.env.NODE_ENV === "production"
+
+        res.cookie("accessToken", token, {
+            httpOnly:true,
+            secure:isProd,
+            sameSite: isProd ? "none" : "lax",
+            path: "/",
+            maxAge: 10* 60 * 1000, // 10min
+        })
+
+        res.status(200).json({
+            error:false,
+            messege:"Login successful",
+            token:token,
+            user:{
+                _id: user._id,
+                username: user.username,
+                email: user.email,
+                role: user.role,
+            },
+        });
+    } catch (error) {
+        next(error)
+    }
+});
+
+
+//Logout a user
+router.post("/auth/cookie/logout", (req, res) =>{
+    const isProd = process.env.NODE_ENV === "production"
+
+    res.clearCookie("accessToken", {
+        httpOnly:true,
+        secure: isProd,
+        sameSite : isProd ? "none" : "lax",
+        path: "/",
+    });
+
+    res.status(200).json({
+        error: false,
+        message:"Logged out seccesfully",
+    });
+});
+
+
+//Check user authentication (check if user has valid token)
+router.get("/auth/cookie/me", authUser, async (req, res, next) => {
+    try {
+        const userId = req.user.user._id
+
+        const user = await User.findById(userId)
+
+        if(!user){
+            return res.status(401).json({
+                error:true,
+                messega:"Unauthenticated",
+            })
+        }
+
+        res.status(200).json({
+            error: false,
+            user:{
+                _id: user._id,
+                username:user.username,
+                email:user.email,
+                role:user.role,
+            },
+        });
+
+    } catch (error) {
+        next(error)
+    }
+});
+
+
