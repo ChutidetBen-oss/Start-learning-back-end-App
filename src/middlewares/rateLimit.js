dec398ecb64b251c5fcfe08ea42433d59817d3d4
ChutidetBen-oss <chutidet.benchathamloed@gmail.com>
2026-01-09 09:53:41 +0700

    delete: something

--- src/middlewares/rateLimit.js
+++
@@ -1,8 +0,0 @@
-import rateLimite from "express-rate-limit"
-
-export const limiter = rateLimite({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // Limit each IP address to 100 requests per 15 minutes
-    standardHeaders: true,
-    legacyHeaders: false,
-})
--- src/mock-db/users.js
+++
@@ -1,5 +0,0 @@
-export const users = [
-  { id: "1", name: "Alice-mock", email: "alice@example.com" },
-  { id: "2", name: "Bob-mock", email: "bob@example.com" },
-  { id: "3", name: "Charlie-mock", email: "charlie@example.com" },
-];
